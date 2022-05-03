f5977ae752cf79b83ff0c16d2b20e275761c4de5
jlauterb <94076445+jlauterb@users.noreply.github.com>
2022-05-03 16:22:05 -0400

    blah

--- guesswho.py
+++ guesswho.py
@@ -193,7 +193,7 @@ class GuessWho:
         Side effects:
             The player's board is printed
         """
-        print(f"It is {str(self.players[player])} turn!")
+        print(f"{str(self.players[player])} Board: ")
         print(self.players[player].board.keys())
         print("\n")
               
