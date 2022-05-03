c370d870426dc4cfa7ddd96f68d3d4e195b0a70b
Sana Sukhera <ssukhera@terpmail.umd.edu>
2022-05-03 16:27:37 -0400

    print_board

--- guesswho.py
+++ guesswho.py
@@ -193,7 +193,7 @@ class GuessWho:
         Side effects:
             The player's board is printed
         """
-        print(f"It is {str(self.players[player])} turn!")
+        print(f"It is {str(self.players[player])}'s turn! \n")
         [print(k) for k in (self.players[player].board.keys())]
         print("\n")
               
